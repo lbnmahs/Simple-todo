7d456ecc64e4109b840e9941e3f5de2c1d770d49
LbnMahs <labanmahs2002@gmail.com>
2023-01-10 13:03:51 +0300

    Get a quote from an API

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react'
 
 const styles = {
     container: `w-full bg-red-400 m-auto rounded-md shadow-xl p-6 my-8`,
-    date: `font-bold text-white text-2xl text-center`
+    date: `font-medium text-white text-2xl text-center`,
+    quotes: `text-white mt-4`,
+    quoteContent: `font-medium text-base text-left`,
+    quoteAuthor: `font-light text-sm italic text-left`
 }
 
 const Banner = () => {
     const [date, setDate] = useState(new Date())
+    const [quote, setQuote] = useState({})
+    const [timeOfDay, setTimeOfDay] = useState('')
+    const [image, setImage] = useState("");
+
+    // Display day and date
     useEffect(() => {
         const interval = setInterval(() => {
             setDate(new Date());
@@ -19,9 +27,39 @@ const Banner = () => {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
+
+    // Display a quote
+    useEffect(() => {
+        const apiKey = import.meta.env.VITE_QUOTE_API_KEY
+        console.log(apiKey)
+        const getQuote = async() => {
+            const response = await fetch('https://quotes15.p.rapidapi.com/quotes/random/', {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': `${apiKey}`,
+                    'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+                }
+
+            })
+            const data = await response.json()
+            console.log(data)
+            setQuote(data)
+        }
+        const interval = setInterval(getQuote, 1000 * 60 * 60 * 24)
+        getQuote()
+        return () => clearInterval(interval)
+    }, [])
+
+    // Change bg image according to time of day
   return (
     <div className={styles.container}>
       <h1 className={styles.date}>{dayOfWeek}, {day}/{month}/{year}</h1>
+      
+      <div className={styles.quotes}>
+        <p className={styles.quoteContent}>{quote?.content}</p>
+        <p className={styles.quoteAuthor}>~{quote?.originator?.name}</p>
+      </div>
+        
     </div>
   )
 }
