8a8fc738cb786c976759cf11a8ee6a4d7c7f3c35
LbnMahs <labanmahs2002@gmail.com>
2023-01-11 12:38:06 +0300

    Finishing up the banner

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Todo from "./Todo";
 
 
 const styles = {
-  bg: `w-full h-screen p-5 bg-gradient-to-r from-sky-500 to-indigo-500`,
+  bg: `w-full h-screen p-5 bg-slate-800`,
   container: `max-w-[800px] w-full m-auto p-4`,
   form: `flex justify-between mb-8`,
   input: `border p-2 w-full rounded-md text-xl outline-none`,
@@ -17,9 +17,9 @@ function App() {
   const [todos, setTodos] = useState(['Todo 1', 'Todo 2', 'Todo 3'])
   return (
     <div className={styles.bg}>
-      
+      <Banner />
       <div className={styles.container}>
-        <Banner />
+        
         <form className={styles.form}>
           <input className={styles.input} placeholder="Add a new Todo"  />
           <button className={styles.button} type="submit"><AiOutlinePlus size={30}/></button>
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import afternoon from './assets/afternoon.jpg'
+import morning from './assets/morning.jpg'
+import night from './assets/night.jpg'
+import evening from './assets/evening.jpg'
 
 const styles = {
-    container: `w-full bg-red-400 m-auto rounded-md shadow-xl p-6 my-8`,
+    container: `max-w-4xl bg-red-400 m-auto rounded-md shadow-xl p-12 my-8 bg-cover bg-center `,
+    bg: `bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 bg-black rounded-md p-6`,
     date: `font-medium text-white text-2xl text-center`,
     quotes: `text-white mt-4`,
-    quoteContent: `font-medium text-base text-left`,
-    quoteAuthor: `font-light text-sm italic text-left`
+    quoteContent: `font-medium text-xl text-left`,
+    quoteAuthor: `font-light text-base italic text-left`
 }
 
 const Banner = () => {
@@ -31,7 +36,6 @@ const Banner = () => {
     // Display a quote
     useEffect(() => {
         const apiKey = import.meta.env.VITE_QUOTE_API_KEY
-        console.log(apiKey)
         const getQuote = async() => {
             const response = await fetch('https://quotes15.p.rapidapi.com/quotes/random/', {
                 method: 'GET',
@@ -42,23 +46,48 @@ const Banner = () => {
 
             })
             const data = await response.json()
-            console.log(data)
-            setQuote(data)
+            if(data?.originator?.name){
+                setQuote(data)
+                setTimeout(getQuote, 1000 * 60 * 60 * 24)
+            }else{
+                getQuote()
+            }
         }
-        const interval = setInterval(getQuote, 1000 * 60 * 60 * 24)
         getQuote()
-        return () => clearInterval(interval)
+        // const interval = setInterval(getQuote, 1000 * 60 * 60 * 24)
+        // getQuote()
+        // return () => clearInterval(interval)
     }, [])
 
     // Change bg image according to time of day
+    useEffect(() => {
+        const currentTime = date.getHours();
+        if(currentTime < 12){
+            setTimeOfDay('morning')
+            setImage(morning)
+        }else if(currentTime < 17){
+            setTimeOfDay('afternoon')
+            setImage(afternoon)
+        }else if(currentTime < 21){
+            setTimeOfDay('evening')
+            setImage(evening)
+        }else{
+            setTimeOfDay('night')
+            setImage(night)
+        }
+    }, [])
   return (
-    <div className={styles.container}>
-      <h1 className={styles.date}>{dayOfWeek}, {day}/{month}/{year}</h1>
+    <div className={styles.container} style={{ backgroundImage: `url(${image})`, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+        <div className={styles.bg}>
+            <h1 className={styles.date}>Good {timeOfDay}!, {dayOfWeek}, {day}/{month}/{year}</h1>
+            <div className={styles.quotes}>
+                <p className={styles.quoteContent}>{quote?.content}</p>
+                {
+                    quote?.originator?.name && <p className={styles.quoteAuthor}>- {quote?.originator?.name}</p>
+                }
+            </div>
+        </div>
       
-      <div className={styles.quotes}>
-        <p className={styles.quoteContent}>{quote?.content}</p>
-        <p className={styles.quoteAuthor}>~{quote?.originator?.name}</p>
-      </div>
         
     </div>
   )
